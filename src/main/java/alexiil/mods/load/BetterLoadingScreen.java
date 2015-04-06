e3521490353ca8d4d32c9a2d5bd640be089a7d88
AlexIIL <alexj9@me.com>
2015-04-06 22:22:27 +0100

    Actually populate instance field with the correct mod, and update AlexIILLib

--- src/main/java/alexiil/mods/load/BetterLoadingScreen.java
+++ src/main/java/alexiil/mods/load/BetterLoadingScreen.java
@@ -12,10 +12,9 @@ import alexiil.mods.lib.AlexIILMod;
 
 import com.google.common.eventbus.EventBus;
 
-@Mod(modid = "betterloadingscreen", guiFactory = "alexiil.mods.load.ConfigGuiFactory", useMetadata = true, dependencies = "required-after:alexiillib")
+@Mod(modid = Lib.Mod.ID, guiFactory = "alexiil.mods.load.ConfigGuiFactory", useMetadata = true, dependencies = "required-after:alexiillib")
 public class BetterLoadingScreen extends AlexIILMod {
-
-    @Mod.Instance
+    @Mod.Instance(Lib.Mod.ID)
     public static BetterLoadingScreen instance;
 
     @EventHandler
