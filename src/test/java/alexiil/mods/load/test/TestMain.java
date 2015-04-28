96ff0385019e02625a2b50ade5160e71c53bccec
AlexIIL <alexj9@me.com>
2015-04-28 21:38:05 +0100

    Hopefully fix the setup script???

--- src/test/java/alexiil/mods/load/test/TestMain.java
+++ src/test/java/alexiil/mods/load/test/TestMain.java
@@ -4,6 +4,6 @@ public class TestMain {
 
     public static void main(String[] args) {
         TestModExporter.dumpMods();
-        GradleStart.main(args);
+        // GradleStart.main(args);
     }
 }
