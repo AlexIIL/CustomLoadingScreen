42851d7669dc7f231f455ef378651995433dff71
AlexIIL <alexj9@me.com>
2015-05-19 15:16:02 +0100

    Actually update AlexIILLib in the build script

--- src/main/java/alexiil/mods/load/Lib.java
+++ src/main/java/alexiil/mods/load/Lib.java
@@ -2,7 +2,6 @@ package alexiil.mods.load;
 
 public class Lib {
     public static class Mod {
-
         public static final String ID = "betterloadingscreen";
         public static final String NAME = "Better Loading Screen";
         public static final String VERSION = "@VERSION@";
