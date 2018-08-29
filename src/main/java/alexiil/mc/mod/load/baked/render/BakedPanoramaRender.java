d6ce71198e7208f758b3ee9f25172222c479f86b
AlexIIL <alexj9@me.com>
2018-08-29 14:32:33 +0100

    Fix BakedPanoramaRender from crashing if something else uses the main tessellator at the same time.

--- src/main/java/alexiil/mc/mod/load/baked/render/BakedPanoramaRender.java
+++ src/main/java/alexiil/mc/mod/load/baked/render/BakedPanoramaRender.java
@@ -16,6 +16,14 @@ import alexiil.mc.mod.load.render.MinecraftDisplayerRenderer;
 import buildcraft.lib.expression.api.IExpressionNode.INodeDouble;
 
 public class BakedPanoramaRender extends BakedRender {
+
+    /** We only ever render 4 x (3 pos, 2 uv, 1 colour) ints each time then reset for the next face.
+     * <p>
+     * So this 64 is overkill. */
+    private static final int TESS_INT_COUNT = 0x40;
+
+    private final Tessellator tess = new Tessellator(TESS_INT_COUNT);
+
     /** Timer used to rotate the panorama, increases every minecraft tick. (20tps) */
     private double actualAngle;
     private final INodeDouble angleFunc;
@@ -23,11 +31,13 @@ public class BakedPanoramaRender extends BakedRender {
 
     public BakedPanoramaRender(INodeDouble angle, String resourceLocation) {
         String[] strings = new String[6];
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < 6; i++) {
             strings[i] = resourceLocation.replace("_x", "_" + i);
+        }
         cubeSides = new ResourceLocation[6];
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < 6; i++) {
             cubeSides[i] = new ResourceLocation(strings[i]);
+        }
         angleFunc = angle;
     }
 
@@ -43,8 +53,7 @@ public class BakedPanoramaRender extends BakedRender {
     }
 
     private void drawPanorama(MinecraftDisplayerRenderer renderer) {
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder vb = tessellator.getBuffer();
+        BufferBuilder vb = tess.getBuffer();
 
         GlStateManager.matrixMode(GL11.GL_PROJECTION);
         GlStateManager.pushMatrix();
@@ -64,7 +73,8 @@ public class BakedPanoramaRender extends BakedRender {
             float f2 = ((float) (k / b0) / (float) b0 - 0.5F) / 64.0F;
             float f3 = 0.0F;
             GlStateManager.translate(f1, f2, f3);
-            GlStateManager.rotate(MathHelper.sin(((float) this.actualAngle) / 400.0F) * 25.0F + 20.0F, 1.0F, 0.0F, 0.0F);
+            GlStateManager.rotate(MathHelper.sin(((float) this.actualAngle) / 400.0F) * 25.0F + 20.0F, 1.0F, 0.0F,
+                0.0F);
             GlStateManager.rotate(-((float) this.actualAngle) * 0.1F, 0.0F, 1.0F, 0.0F);
 
             for (int l = 0; l < 6; ++l) {
@@ -99,7 +109,7 @@ public class BakedPanoramaRender extends BakedRender {
                 vb.pos(1.0D, -1.0D, 1.0D).tex(1.0F - f4, 0.0F + f4).color(rgb, rgb, rgb, alpha).endVertex();
                 vb.pos(1.0D, 1.0D, 1.0D).tex(1.0F - f4, 1.0F - f4).color(rgb, rgb, rgb, alpha).endVertex();
                 vb.pos(-1.0D, 1.0D, 1.0D).tex(0.0F + f4, 1.0F - f4).color(rgb, rgb, rgb, alpha).endVertex();
-                tessellator.draw();
+                tess.draw();
                 GlStateManager.popMatrix();
             }
 
