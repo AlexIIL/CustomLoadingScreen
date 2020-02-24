645cca21b68e64d35b13efa1a409b43330cfdbd6
AlexIIL <alexj9@me.com>
2020-02-24 17:33:39 +0000

    Fix other languages not displaying correctly by actually binding the right font texture when rendering them.
    (Fixes #60)

--- src/main/java/alexiil/mc/mod/load/render/FontRendererSeparate.java
+++ src/main/java/alexiil/mc/mod/load/render/FontRendererSeparate.java
@@ -16,20 +16,23 @@ import net.minecraft.client.resources.IResource;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraft.util.ResourceLocation;
 
-public class FontRendererSeparate extends FontRenderer {
+import alexiil.mc.mod.load.CLSLog;
 
+public class FontRendererSeparate extends FontRenderer {
     private final Map<ResourceLocation, BufferedImage> textureData = new HashMap<>();
     private final Map<ResourceLocation, Integer> textureLocations = new HashMap<>();
 
-    public FontRendererSeparate(GameSettings settings, ResourceLocation location, TextureManager textureManagerIn,
-        boolean unicode) {
+    public FontRendererSeparate(
+        GameSettings settings, ResourceLocation location, TextureManager textureManagerIn, boolean unicode
+    ) {
         super(settings, location, textureManagerIn, unicode);
 
         loadTex(location);
-        // TODO: Load unicode pages?
-        // for (int i = 0; i < 256; i++) {
-
-        // }
+        for (int i = 0; i < 256; i++) {
+            if (i == 8) continue;
+            if (0xd8 <= i && i <= 0xf8) continue;
+            loadTex(new ResourceLocation(String.format("textures/font/unicode_page_%02x.png", i)));
+        }
     }
 
     private void loadTex(ResourceLocation location) {
@@ -52,6 +55,7 @@ public class FontRendererSeparate extends FontRenderer {
         if (value == null) {
             BufferedImage img = textureData.get(location);
             if (img == null) {
+                CLSLog.warn("Non-cached texture: '" + location + "'");
                 return;
             }
             int next = GL11.glGenTextures();
